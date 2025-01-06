efa79a555051ad3cf39368c71fc1ed721c974871
Kevin Durbin <kevindurb@fastmail.com>
2025-01-06 09:51:25 -0700

    get shell working

--- packages/app-shell/src/app-shell.js
+++ packages/app-shell/src/app-shell.js
@@ -1,14 +1,28 @@
 import { html, LitElement } from 'lit';
-import { Routes } from '@lit-labs/router';
+import { Router } from '@lit-labs/router';
 
 class AppShell extends LitElement {
-  #routes = new Routes(this, [
-    { path: '/people/*', render: () => html`<people-mfe/>`, enter: () => import('person-mfe') },
-    { path: '/groups/*', render: () => html`<groups-mfe/>`, enter: () => import('group-mfe') },
+  #router = new Router(this, [
+    {
+      path: '/',
+      enter: () => {
+        window.location.href = '/people';
+      },
+    },
+    {
+      path: '/people*',
+      render: () => html`<people-mfe />`,
+      enter: () => import('person-mfe'),
+    },
+    {
+      path: '/groups*',
+      render: () => html`<groups-mfe />`,
+      enter: () => import('group-mfe'),
+    },
   ]);
 
   render() {
-    return html`${this.#routes.outlet()}`;
+    return html`${this.#router.outlet()}`;
   }
 }
 
